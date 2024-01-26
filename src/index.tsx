a55d0c03212e613643eb8d1746d8230a28007b75
Yahor Yankavets <mr.yankovec@gmail.com>
2024-01-26 19:47:11 +0300

    connected redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
